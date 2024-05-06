9f14df3d64c3664a1d709da82e0f09bea2893707
cetariqfarooq <93148293+cetariqfarooq@users.noreply.github.com>
2024-05-06 18:36:26 +0000

    container config done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,1 +1,20 @@
+const account_id = 144553
+let account_email = "cetariqfarooq@gmail.com"
+var account_password = "12345"
+account_city = "SGD"
+// account_id= 3569 // not allowed
+console.log(account_id);
+console.log(account_email);
+console.log(account_password);
+console.log(account_city);
+let account_state;
 
+account_email = "cstariqfarooq@gmail.com"
+// console.log(account_email);
+
+console.table([account_id, account_email, account_password, account_city])
+/*
+prefered not ti use var for variable
+var is not used due to block scope and functional scope issues
+in JS, is a variable is declared only without assigning any value then it will be shown as Undefined
+*/
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+
+"use strict"; // treat all JS code as newer version of JS
+// alert(3 + 3) // we are using node js, not browser where this code runs successfully
+let name = "umer"
+let age = 29
+let state;
+/*
+In JS, we have two types of data types; 
+Primitive (call by value) and these are
+1. number (integer or float)
+2. string
+3. boolean
+4. BigInt
+5. Undefied
+6. Null; a standalone value
+7. symbol; for unique values
+
+Non-Primitives (call by reference) and thes are;
+1. Object (similar to dictionary in Python)
+2. Function
+3. Array
+*/
