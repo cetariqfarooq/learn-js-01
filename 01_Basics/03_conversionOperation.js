adf0fc899b04add54f5a994433c6d6cfee0af69e
cetariqfarooq <93148293+cetariqfarooq@users.noreply.github.com>
2024-05-07 17:01:09 +0000

    container config done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = null
+// console.log(typeof score);
+let value_in_number = Number(score)
+// console.log(typeof(value_in_number));
+
+/*
+"33" is converted into a Number
+"33abc" is also converted into a Number but value is NaN
+true returns 1 and false return 0
+*/
+
+let is_logged_in = 1
+let bool_is_logged_in = Boolean(is_logged_in)
+console.log(bool_is_logged_in);
+
+/*
+1 is converted into True nd 0 is converted into false
+empy string "" means false and "TF" means true
+*/
