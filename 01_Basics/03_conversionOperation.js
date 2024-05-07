e1e5f6f8aeeb863947fdf3bf8d1f9f906b428b11
cetariqfarooq <93148293+cetariqfarooq@users.noreply.github.com>
2024-05-07 17:17:08 +0000

    conversion done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -11,9 +11,14 @@ true returns 1 and false return 0
 
 let is_logged_in = 1
 let bool_is_logged_in = Boolean(is_logged_in)
-console.log(bool_is_logged_in);
+// console.log(bool_is_logged_in);
 
 /*
 1 is converted into True nd 0 is converted into false
 empy string "" means false and "TF" means true
 */
+
+//******************** Operations ************************//
+let val = 3
+let neg_val = - val
+console.log(neg_val);
