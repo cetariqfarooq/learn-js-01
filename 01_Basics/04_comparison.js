d7b28fd0168f4e7bb1f05a9ae254085de41f4eac
cetariqfarooq <93148293+cetariqfarooq@users.noreply.github.com>
2024-05-07 17:27:42 +0000

    comparison done

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log(2 > 1);
+// console.log("2" > 1);
+// console.log(null > 1);
+// console.log(null >= 1);
+// console.log(null < 1);
+// console.log(null == 1);
+
+// === is used for strict check for checking data type as follows
+console.log("2" == 2); // answer is true
+console.log("2" === 2); // answer is false because data type and comprison, both are being checked
+
