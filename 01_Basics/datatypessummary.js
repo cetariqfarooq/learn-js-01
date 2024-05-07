5d8908f7a790e6a14b50072a886d8bb3a6f610e1
cetariqfarooq <93148293+cetariqfarooq@users.noreply.github.com>
2024-05-07 17:56:31 +0000

    summary done

---
+++ 01_Basics/datatypessummary.js
@@ -0,0 +1,10 @@
+// primitive for stack memory and heap for non-primitive data types in JS
+let youtube_channel = "TariqFarooq"
+let another_name = youtube_channel
+console.log(youtube_channel);
+console.log(another_name);
+// after manipulation
+another_name = "New Name"
+console.log("After Changes");
+console.log(youtube_channel);
+console.log(another_name);
