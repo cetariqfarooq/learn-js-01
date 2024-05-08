63dc1b0ceeadf14c0cd273ae00a78121da68127f
cetariqfarooq <93148293+cetariqfarooq@users.noreply.github.com>
2024-05-08 17:51:48 +0000

    sumary done

--- 01_Basics/datatypessummary.js
+++ 01_Basics/datatypessummary.js
@@ -1,10 +1,33 @@
 // primitive for stack memory and heap for non-primitive data types in JS
 let youtube_channel = "TariqFarooq"
 let another_name = youtube_channel
-console.log(youtube_channel);
-console.log(another_name);
+// console.log(youtube_channel);
+// console.log(another_name);
 // after manipulation
 another_name = "New Name"
-console.log("After Changes");
-console.log(youtube_channel);
-console.log(another_name);
+// console.log("After Changes");
+// console.log(youtube_channel);
+// console.log(another_name);
+
+const id = Symbol("123")
+const new_id = Symbol("123")
+// console.log(id === new_id);
+const big_number = 123456789123456789n
+// console.log(big_number);
+// console.log(typeof big_number);
+
+const heros = ["Captain America", "Thor", "Iron Man", "Hulk"]
+// console.log(heros);
+let std_registration ={
+    name:"UF",
+    id:12345,
+    age:29
+}
+// console.log(std_registration["name"]);
+
+function intro(params) {
+    console.log("Hello Function");
+}
+intro()
+
+
