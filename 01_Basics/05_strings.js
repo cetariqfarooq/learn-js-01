970114bfb12d71f1c19d5f35f883ef9521e76fa3
cetariqfarooq <93148293+cetariqfarooq@users.noreply.github.com>
2024-05-08 18:42:28 +0000

    string done

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -8,4 +8,17 @@ const first_name = "Hassaan"
 const second_name = " Tariq"
 const full_name = first_name + second_name
 // console.log(full_name);
-console.log(`"Your name is ${first_name} ${second_name}"`);
+// console.log(`Your name is ${first_name} ${second_name}`);
+const game_name = new String("Umer Farooq")
+// console.log(game_name);
+// console.log(typeof game_name);
+// console.log(game_name.charAt(3));
+// console.log(game_name.toLowerCase());
+// console.log(game_name.toUpperCase());
+// console.log(game_name.trim());
+// console.log(game_name.length);
+
+// console.log(game_name.indexOf("U"));
+
+// console.log(game_name.substring(0, 6));
+console.log(game_name.slice(-1, 3));
